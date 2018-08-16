06df5eea18d72c1169fd6412ff16e4afa7087e99
Isadora Bassetto Kwiatkowski <isadorabk@gmail.com>
2018-08-16 15:30:45 +0200

    feat: createIngredient and getIngredients

---
+++ controllers/ingredients.controller.js
@@ -0,0 +1,85 @@
+'use strict';
+
+const db = require('../models').db;
+const filterProps = require('../services/utils.js').filterProps;
+
+
+class IngredientsController {
+  constructor (ingredientModel) {
+    if (!ingredientModel) throw new Error('Ingredient model not provided');
+    this.Ingredient = ingredientModel;
+    this.createIngredient = this.createIngredient.bind(this);
+    this.getIngredients = this.getIngredients.bind(this);
+  }
+
+  async createIngredient (ctx, next) {
+    // Check if the method is correct
+    if (ctx.method !== 'POST') throw new Error('Method not allowed');
+
+    const data = ctx.request.body;
+    //Check if the request has email and password
+    if (data.name) {
+      data.name = data.name.toLowerCase();
+      // Check if there's already an ingredient with this name
+      let ingredient = await this.Ingredient.findOne({
+        where: {
+          name: data.name
+        }
+      });
+      // if there's already a ingredient, send an error
+      if (ingredient) {
+        ctx.status = 401;
+        ctx.body = {
+          errors: ['Ingredient already exists.']
+        };
+      } else {
+        // If there's no ingredient, create a new one
+        ingredient = filterProps(data, ['name', 'ingredient_type_id']);
+        let newIngredient = await this.Ingredient.create(ingredient);
+
+        ctx.body = filterProps(newIngredient.dataValues, ['id', 'name']);
+        ctx.status = 201;
+      }
+    } else {
+      ctx.status = 406;
+      ctx.body = {
+        errors: ['Name of ingredient needed']
+      };
+      return;
+    }
+  }
+
+  async getIngredients (ctx, next) {
+    // Check if the method is correct
+    if (ctx.method !== 'GET') throw new Error('Method not allowed');
+
+    // Find all ingredients
+    const ingredients = await this.Ingredient.findAll({
+      include: [{
+        model: db.Ingredient_type
+      }]
+    });
+
+    if (ingredients) {
+      ctx.body = [];
+      ingredients.forEach(el => {
+        const ingredient = {
+          id: el.id,
+          name: el.name,
+          ingredient_type: el.dataValues.Ingredient_type.dataValues.name
+        };
+        ctx.body.push(ingredient);
+      });
+      ctx.status = 200;
+    } else {
+      // Send an error if there's no ingredient
+      ctx.status = 404;
+      ctx.body = {
+        errors: ['No ingredient was found.']
+      };
+      return;
+    }
+  }
+}
+
+module.exports = IngredientsController;
--- routes/routes.js
+++ routes/routes.js
@@ -6,13 +6,21 @@ const authMiddleware = require('../middlewares/authorization.js');
 const db = require('../models').db;
 db.setup();
 
-const UsersController = require('../controllers/user.controller');
+const UsersController = require('../controllers/users.controller');
 const usersController = new UsersController(db.User);
+
+const IngredientsController = require('../controllers/ingredients.controller');
+const ingredientsController = new IngredientsController(db.Ingredient);
 
 // User routes
 router
   .post('/users', usersController.createUser)
   .post('/sign-in', usersController.signIn)
   .get('/me', authMiddleware, usersController.getUser);
+
+// Ingredient routes
+router
+  .post('/ingredients', authMiddleware, ingredientsController.createIngredient)
+  .get('/ingredients', authMiddleware, ingredientsController.getIngredients);
 
 module.exports = router;
